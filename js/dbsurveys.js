e041f5ec1158928cf8fce8b963874c988a011def
williamdelrosario <williamdelrosario@yahoo.com>
2017-12-09 20:10:47 +0100

    Completed surveys' functions

--- js/dbsurveys.js
+++ js/dbsurveys.js
@@ -61,6 +61,26 @@ db.surveys
         console.log ("Got from active status");
 });
 
+/* Update */
+db.surveys
+    .update(8, {name: "closed"}).then(function (updated) {
+        if (updated)
+    console.log ("Survey number 2 was renamed closed");
+        else
+    console.log ("Nothing was updated - there were no survey with primary key: closed");
+});
+
+/* Update option2
+db.surveys
+    .update()
+    .where('status')
+    .equals('active')
+    .toArray()
+    .then(function (survey) {
+        console.log ("Closed active status");
+});
+*/
+
 /* getById */
 db.surveys
     //.each(callbackFn)
@@ -71,7 +91,33 @@ db.surveys
         console.log ("Got from id");
 });
 
+    // Populate Survey table
+db.transaction('rw', db.surveys, function () {
+    db.surveys
+    .add({
+        questionlist_id: "questionlist_id",
+        name: "name",
+        type: "type",
+        status: "status",
+        location_id: "location_id",
+        progress: "progress",
+        edited: "moment().format('YYYY-MM-DD HH:mm:ss')",
+        questions:"questions",
+        confirmation: "confirmation",
+        email_cc: "email_cc"
+    });
+}).catch (function (e) {
+    console.error(e.stack);
+});
 
+/* update options = {status,location_id,progress,questions} */
+db.surveys
+    .update(8, {name: "closed"}).then(function (updated) {
+        if (updated)
+    console.log ("Survey number 2 was renamed closed");
+        else
+    console.log ("Nothing was updated - there were no survey with primary key: closed");
+});
 
 
 
